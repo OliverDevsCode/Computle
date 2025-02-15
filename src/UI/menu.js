963438fcd509e0e30ae588e7b6c844b3fe51bff8
OliverDevsCode <186149954+OliverDevsCode@users.noreply.github.com>
2025-02-15 13:20:28 +0000

    user notified of score reset

--- src/UI/menu.js
+++ src/UI/menu.js
@@ -79,26 +79,35 @@ function drawMenu(user){
   validateButton.mousePressed(validatePreviousScoreMenu)
 }
 
-function changeSubjectMenu(){
-    closeMenu()
-    mode = 2
-    let subjects = getSubjects();
-    background(canvasColour);
-    //create a dropdown menu
-    let subjectSelect = createDropDown(subjects,width/6,height/1.5,300,50)
-    let enterButton = createCustomButton("SELECT",width/6 + 320,height/1.5,"#00FF00")
-    push()
-    fill(255,0,255)
-    textAlign(CENTER)
-    textFont("Inter")
-    textStyle(BOLD)
-    textSize(width/15)
-    text("Welcome To Computle!",width/2,height/4)
-    textSize(width/20)
-    text("Select A Topic Below",width/2,height/2)
-    pop()
+let alerted = false
 
-    enterButton.mousePressed(() => startcomputle(subjectSelect,enterButton,subjects));
+function changeSubjectMenu(){
+    if(alerted == false){
+      alert("Changing Subject Will Reset Score")
+      alerted = true
+    }else{
+      closeMenu()
+      mode = 2
+      let subjects = getSubjects();
+      background(canvasColour);
+      //create a dropdown menu
+      let subjectSelect = createDropDown(subjects,width/6,height/1.5,300,50)
+      let enterButton = createCustomButton("SELECT",width/6 + 320,height/1.5,"#00FF00")
+      push()
+      fill(255,0,255)
+      textAlign(CENTER)
+      textFont("Inter")
+      textStyle(BOLD)
+      textSize(width/15)
+      text("Welcome To Computle!",width/2,height/4)
+      textSize(width/20)
+      text("Select A Topic Below",width/2,height/2)
+      pop()
+  
+      enterButton.mousePressed(() => startcomputle(subjectSelect,enterButton,subjects));
+      alerted = false
+    }
+    
 }
 
 function closeMenu(){
