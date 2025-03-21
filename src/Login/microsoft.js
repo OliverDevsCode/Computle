ee6e9fd3c7919a69d17dec98a0a87e01d5d4d5f7
OliverDevsCode <186149954+OliverDevsCode@users.noreply.github.com>
2025-03-21 20:02:34 +0000

    connection fix

--- src/Login/microsoft.js
+++ src/Login/microsoft.js
@@ -3,9 +3,9 @@ import { PublicClientApplication } from '@azure/msal-browser';
 // MSAL configuration object
 const msalConfig = {
     auth: {
-        clientId: process.env.AZURE_CLIENT_ID, // Your Application (client) ID from Entra ID
-        authority: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}`, // Tenant-specific endpotint
-        redirectUri: process.env.AZURE_REDIRECT_URI // Must match your registered redirect URI
+        clientId: 'db78800c-5db9-4c80-8e6b-c3ba7ab1a454', // Your Application (client) ID from Entra ID
+        authority: `https://login.microsoftonline.com/737b9d9f-9b7b-4dd2-b7d3-aef85aef5565`, // Tenant-specific endpotint
+        redirectUri: 'https://computle.vercel.app' // Must match your registered redirect URI
     },
     cache: {
         cacheLocation: "sessionStorage", // Options: "sessionStorage" or "localStorage"
